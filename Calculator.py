49663337527e4a1000aa7bb44fe6a59c6b6426f3
dennislin <w4559731234@gmail.com>
2019-11-07 17:12:54 +0800

    1.0

--- Calculator.py
+++ Calculator.py
@@ -1,7 +1,17 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Nov  7 15:56:37 2019
+#master
 
-@author: 301
-"""
-print("hi")
+#示範Git用的程式碼！
+
+def add(a, b):
+    return a + b
+
+def minus(a, b):
+    return a - b
+
+def times(a, b):
+    return a * b
+
+def divided(a, b):
+    if b != 0:
+        return a / b
+    return "無限"
